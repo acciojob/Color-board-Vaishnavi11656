16482d0881af2606a508a57b7c04bf4cf2c22ed5
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2025-12-15 00:23:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const container = document.querySelector(".container")
+
+            // Create 800 squares
+            for(let i = 1; i<=800; i++){
+                console.log("loop",i)
+                createSquare()
+            }
+
+
+            function createSquare(){
+                const square = document.createElement("div")
+                square.classList.add("square")
+                square.addEventListener("mouseover",addRandomColor)
+                square.addEventListener("mouseout",removecolor);
+                container.appendChild(square)
+            }  
+            function addRandomColor(e){
+                const square=e.target
+                const randomColor= "#"+ Math.floor(Math.random()*255*255*255).toString(16).padStart(6,"0")
+                console.log(randomColor,randomColor.length,'random color')
+                square.style.backgroundColor=randomColor
+            }
+            function removecolor(e){
+                setTimeout(()=>{
+                    e.target.style.backgroundColor = "";
+                   // e.target.style.transition
+                },1000)
+            }
